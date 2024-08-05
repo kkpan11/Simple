ea1208154e24504a7a98144a702d4c7ca2420442
Cliff Click <cliffc@acm.org>
2024-08-05 16:15:27 -0700

    Chapter 13: References
    Co-authored-by: Dibyendu Majumdar <mobile@majumdar.org.uk>
    Co-authored-by: Thalia Archibald <thalia@archibald.dev>
    Co-authored-by: XmiliaH <45106915+XmiliaH@users.noreply.github.com>
    Co-authored-by: Robert Obkircher <robert.obkircher@gmail.com>

--- src/test/java/com/seaofnodes/simple/Chapter11Test.java
+++ src/test/java/com/seaofnodes/simple/Chapter11Test.java
@@ -324,7 +324,7 @@ while( prime <= arg ) {
 return primeCount;
 """);
         StopNode stop = parser.parse(false).iterate(true);
-        assertEquals("Stop[ return 0; return Phi(Loop19,1,Phi(Region81,Phi_primeCount,Phi(Region76,(Phi_primeCount+1),Phi_primeCount))); ]", stop.toString());
+        assertEquals("Stop[ return 0; return Phi(Loop20,1,Phi(Region87,Phi_primeCount,Phi(Region81,(Phi_primeCount+1),Phi_primeCount))); ]", stop.toString());
         assertEquals(0L, Evaluator.evaluate(stop,  1)); // No primes 1 or below
         assertEquals(1L, Evaluator.evaluate(stop,  2)); // 2
         assertEquals(2L, Evaluator.evaluate(stop,  3)); // 2, 3
@@ -366,7 +366,7 @@ if (arg) {
 return i;
 """);
         StopNode stop = parser.parse(false).iterate(true);
-        assertEquals("return Phi(Region31,.f,0);", stop.toString());
+        assertEquals("return Phi(Region34,.f,0);", stop.toString());
     }
 
     @Test
@@ -472,7 +472,7 @@ while(arg) {
 return arg;
 """);
         StopNode stop = parser.parse(false).iterate(false);
-        assertEquals("return Phi(Loop13,arg,Phi(Region35,.f,0));", stop.toString());
+        assertEquals("return Phi(Loop15,arg,Phi(Region37,.f,0));", stop.toString());
     }
 
     @Test
@@ -523,7 +523,7 @@ else if (arg == 1) s.f = 1;
 return s.f;
 """);
         StopNode stop = parser.parse(false).iterate(true);
-        assertEquals("return Phi(Region35,1,Phi(Region33,1,0));", stop.toString());
+        assertEquals("return Phi(Region37,1,Phi(Region35,1,0));", stop.toString());
         assertEquals(1L, Evaluator.evaluate(stop,  0));
         assertEquals(1L, Evaluator.evaluate(stop,  1));
         assertEquals(0L, Evaluator.evaluate(stop,  2));
